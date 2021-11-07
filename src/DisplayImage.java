86095397eb4f1acb0758205d20667c4717b72ce3
Ryan Milewski <rmilewski44@gmail.com>
2021-11-06 22:58:34 -0500

    Added Box to Choose Sat Type
    Co-authored-by: EliBoll <Eliboll@users.noreply.github.com>

--- src/DisplayImage.java
+++ src/DisplayImage.java
@@ -2,7 +2,6 @@
 import java.awt.image.*;
 import java.io.IOException;
 import javax.swing.*;
-import javax.swing.event.*;
 import java.awt.*;
 import java.util.Hashtable;
 
@@ -14,18 +13,20 @@ public class DisplayImage {
     JSlider slide;
     JTextArea textArea;
     JScrollPane scrollableTextArea;
+    JComboBox categories;
     int zoom = 8;
+    int category = 0;
+    String[] cats = { "Select Satellite Type", "All Categories", "Brightest Satellites", "Starlink", "Military", "GPS",
+            "Weather", "Satellite Radio (Sirius XM)", "Television", "Scientific", "Iridium" };
+    int[] cat_ids = { 0, 0, 1, 52, 30, 20, 3, 33, 34, 26, 15 };
 
     public DisplayImage() throws IOException {
-
         frame = new JFrame();
         frame.setSize(1000, 1000);
         key = new JLabel();
         lbl = new JLabel();
         frame.setLayout(new FlowLayout());
-        // frame.setLayout(new GridLayout());
         key.setFont(new Font("Calibri", Font.BOLD, 30));
-
         textArea = new JTextArea(20, 20);
         Font font = new Font("Calibri", Font.BOLD, 20);
         textArea.setFont(font);
@@ -51,25 +52,18 @@ public class DisplayImage {
         slide.setPaintLabels(true);
         slide.setSnapToTicks(true);
         frame.add(slide);
-        JLabel filler = new JLabel();
-        filler.setText("         Satellite Tracker (TM)");
-        filler.setFont(new Font("Calibri", Font.BOLD, 30));
-        frame.add(filler);
-
+        categories = new JComboBox(cats);
+        categories.setSelectedIndex(0);
+        categories.setPreferredSize(new Dimension(350, 20));
+        frame.add(categories);
     }
 
     public void refresh(BufferedImage img, String compass) throws IOException {
         frame.setVisible(true);
-        if (zoom == 7) {
-            key.setFont(new Font("Calibri", Font.BOLD, 10));
-        } else {
-            key.setFont(new Font("Calibri", Font.BOLD, 20));
-        }
         icon = new ImageIcon(img);
         key.setText(compass);
 
         textArea.setText(compass);
-        // textArea.append(compass);
 
         lbl.setIcon(icon);
 
@@ -77,6 +71,11 @@ public class DisplayImage {
         frame.repaint();
         frame.setSize(img.getWidth() + 400, img.getHeight() + 140);
         zoom = slide.getValue();
+        for (int i = 0; i < cats.length; i++) {
+            if (cats[i].equals(categories.getSelectedItem().toString())) {
+                category = cat_ids[i];
+            }
+        }
     }
 
 }
